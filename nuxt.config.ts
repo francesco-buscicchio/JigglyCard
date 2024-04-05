33da019f590b93cf2df04ffa50f50e88056deab2
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-04-05 17:48:32 +0200

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,17 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-
+  runtimeConfig: {
+    public: {
+      FB_API_KEY: process.env.FB_API_KEY,
+      FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+      FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+      FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+      FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+      FB_APP_ID: process.env.FB_APP_ID,
+      FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID,
+    },
+  },
   css: ["~/assets/css/main.scss"],
   modules: ["@nuxt/image", "nuxt-icon"],
 });
