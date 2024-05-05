85cbf6fcb870ccad14dbc41e4e390cf5dccbdb1f
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-05-05 10:26:16 +0200

    Integrazione Koalendar

--- data/newsBanner.ts
+++ data/newsBanner.ts
@@ -1,7 +1,7 @@
 export default [
   {
     label: "💸 CLICCA QUI PER VENDERE LA TUA COLLEZIONE 💸",
-    url: "/card",
+    url: "/valuta-la-tua-collezione",
   },
   {
     label: "💲 CODICE WELCOMEJIGGLY PER AVERE UNO SCONTO DEL 10% 💲",
