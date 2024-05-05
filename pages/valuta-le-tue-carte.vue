56075b14845f2858fd966743832c6238eac161b8
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-05-05 10:17:40 +0200

    Integrazione Koalendar

---
+++ pages/valuta-le-tue-carte.vue
@@ -0,0 +1,12 @@
+<template>
+  <HeaderMobile class="w-full lg:hidden" :header="{ cartCount: 9 }" />
+  <HeaderDesktop class="hidden w-full lg:block" :header="{ cartCount: 9 }" />
+  <iframe
+    src="https://koalendar.com/e/valuta-le-tue-carte?embed=true"
+    width="100%"
+    height="800px"
+    frameborder="0"
+  ></iframe>
+</template>
+
+<script setup lang="ts"></script>
