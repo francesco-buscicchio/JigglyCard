feb04cfc83eb95683b7b1715615cfb41f5dd4888
GaetanoDiNatale <137878215+GaetanoDiNatale@users.noreply.github.com>
2024-05-06 09:19:19 +0200

    fix;test

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="gap-2">
+        <div class="flex w-full">
+            <div class="h-30 flex bg-lightPink w-full">
+                <p>lorem jvbruyveijb</p>
+            </div>
+            <div class="h-30 flex bg-lightPink w-full">
+                <p>lorem jvbruyveijb</p>
+            </div>
+            <div class="h-30 flex bg-lightPink w-full">
+                <p>lorem jvbruyveijb</p>
+            </div>
+        </div>
+        <h1>WOW</h1>
+    </div>
+  </template>
+  
+<script setup lang="ts">
+
+</script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -29,6 +29,7 @@
       </div>
     </div>
   </div>
+  <Footer />
 </template>
 
 <script setup lang="ts">
