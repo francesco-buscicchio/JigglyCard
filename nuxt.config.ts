aebd7dfe20357f8a3c4ba0f8a76d22af1c7fe264
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-05-12 21:10:33 +0200

    Revert "Init"
    This reverts commit bc366523b53f7c9c4db8db351b2385f8503d839a.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,25 +9,18 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-
   runtimeConfig: {
     public: {
-      "graphql-client": {
-        clients: {
-          default: {
-            host: process.env.SHOPIFY_DOMAIN,
-            token: {
-              name: "X-Shopify-Storefront-Access-Token",
-              value: process.env.SHOPIFY_STOREFRONT_TOKEN,
-              type: null,
-            },
-            retainToken: true,
-          },
-        },
-      },
+      FB_API_KEY: process.env.FB_API_KEY,
+      FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+      FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+      FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+      FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+      FB_APP_ID: process.env.FB_APP_ID,
+      FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID,
     },
   },
-  css: ["~/assets/css/main.scss", "nuxt-graphql-client"],
+  css: ["~/assets/css/main.scss"],
   modules: [
     "@nuxt/image",
     "nuxt-icon",
