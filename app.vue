d486c8cf1d0df7f9711036b5abfdceee54ed53d6
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-05-20 16:14:13 +0200

    Pagina 404 (#23)

--- app.vue
+++ app.vue
@@ -2,3 +2,5 @@
   <NuxtLayout> </NuxtLayout>
   <NuxtPage />
 </template>
+
+<script setup lang="ts"></script>
---
+++ error.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="error-container">
+    <img
+      src="~/assets/img/404-image.avif"
+      alt="Lost in a fantasy forest"
+      class="error-image"
+    />
+    <h1 class="text-xl font-bold pt-10">404 - Pagina Non Trovata</h1>
+    <p class="text-lg">
+      Sembra che tu ti sia perso nel bosco delle illusioni... cerca di ritrovare
+      la strada!
+    </p>
+    <nuxt-link to="/" class="home-link">Torna alla Home</nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+.error-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  text-align: center;
+  padding: 15px;
+}
+
+.error-image {
+  max-width: 90%;
+  height: auto;
+}
+
+.home-link {
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #ffcb05; /* Colore giallo per rimanere in tema */
+  color: #2a75bb; /* Colore blu per il testo */
+  border: none;
+  border-radius: 5px;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+@media (min-width: 768px) {
+  .error-image {
+    max-width: 50%;
+  }
+}
+</style>
