e1acf8ea2fc345347e6e8843580432abbad06563
francescobuscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-06-14 22:26:09 +0200

    Test

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -1,56 +1,61 @@
 <template>
-    <div class="flex flex-col items-center space-y-4 mt-mid">
+  <div class="flex flex-col items-center space-y-4 mt-mid">
+    <img
+      src="~/assets/img/logo.png"
+      alt="Logo"
+      class="w-10 md:w-40 cursor-pointer"
+    />
+    <div class="w-full flex items-center justify-center relative">
       <img
-        src="~/assets/img/logo.png"
-        alt="Logo"
-        class="w-10 md:w-40 cursor-pointer"
+        src="~/assets/gif/pokeball.gif"
+        alt="Animated GIF"
+        class="absolute left-0 w-22 md:w-100 cursor-pointer"
       />
-      <div class="w-full flex items-center justify-center relative">
-        <img
-          src="~/assets/gif/pokeball.gif"
-          alt="Animated GIF"
-          class="absolute left-0 w-22 md:w-100 cursor-pointer"
-        />
-        <div class="pt-8">
-            <p class="text-lightPink text-3xl md:text-8xl"> ᒍIGGᒪIᑕᗩᖇᗪ </p>
-        </div>
+      <div class="pt-8">
+        <p class="text-lightPink text-3xl md:text-8xl">JIGGLYCARD</p>
       </div>
-      <h2>{{prestoOnline}}</h2>
     </div>
-  
-    <div class="absolute mb-newsletter w-full">
-        <StaticNewsLetter />
-    </div>
-  </template>
+    <h2>{{ prestoOnline }}</h2>
+  </div>
+
+  <div class="absolute mb-newsletter w-full">
+    <StaticNewsLetter />
+  </div>
+</template>
   
   <script setup lang="ts">
-  import { useI18n } from "vue-i18n";
-    const { t } = useI18n();
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+const api = "https://server.serverjigglycards.com/store/products";
 
-    const prestoOnline = t("prestoOnline");
+const test = fetch(api).then((val) => {
+  console.log(val);
+});
 
-    definePageMeta({
-      layout: ''
-    })
-  </script>
+const prestoOnline = t("prestoOnline");
+
+definePageMeta({
+  layout: "",
+});
+</script>
   
   <style scoped>
-  .mt-mid {
-    margin-top: 10vh;
-  }
+.mt-mid {
+  margin-top: 10vh;
+}
 
-  .w-title{
-    width: 10vh;
-  }
+.w-title {
+  width: 10vh;
+}
 
+.mb-newsletter {
+  bottom: 15vh;
+}
+
+@media screen and (min-width: 700px) {
   .mb-newsletter {
-    bottom: 15vh;
+    bottom: 10vh;
   }
-
-  @media screen and (min-width: 700px) {
-    .mb-newsletter {
-      bottom: 10vh;
-    }
-  }
-  </style>
+}
+</style>
   
