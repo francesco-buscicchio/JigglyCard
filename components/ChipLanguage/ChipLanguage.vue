1c7a973404e5085287b824975d860f3861bc30e7
GaetanoDiNatale <137878215+GaetanoDiNatale@users.noreply.github.com>
2024-06-23 21:14:06 +0200

    bugfix producthero

---
+++ components/ChipLanguage/ChipLanguage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <p class="text-xs mt-2 l:mt-0 mb-2"><strong>{{ label }}</strong></p>
+      <div class="flex flex-row gap-x-4 ">
+        <div v-for="variant in variants" :key="variant.id">
+          <button 
+            :class="['h-10 px-2 border border-gray hover:border-mediumPink focus:border-mediumPink active:border-mediumPink rounded', { 'bg-mediumPink text-white': variant.id === selectedVariantId }]"
+            @click="selectVariant(variant)"
+          >
+            <p>{{ variant.title }}</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits, ref } from 'vue';
+  
+  const props = defineProps({
+    label: String,
+    variants: Array,
+  });
+  
+  const emits = defineEmits(['variantClicked']);
+  
+  const selectedVariantId = ref(null);
+  
+  function selectVariant(variant) {
+    selectedVariantId.value = variant.id;
+    emits('variantClicked', variant);
+  }
+  </script>
+  
+  
--- components/ProductHero/ProductHero.vue
+++ components/ProductHero/ProductHero.vue
@@ -7,36 +7,33 @@
               :src="image.url" 
               :alt="'error'" 
               @click="showImg(image.url)"
-              class="cursor-pointer mb-10 max-w-[100px] border-2 border-transparent"
+              class="cursor-pointer mb-10 max-w-[100px] border-2 border-transparent hover:border-mediumPink"
               :class="{ 'border-2': selectedImage === image.url }"
             />
           </div>
         </div>
         <div class="px-10">
-          <img :src="selectedImage || firstProduct.images[0].url" :alt="'error'" class="max-w-[250px] md:max-w-[500px] h-auto" />
+          <img :src="selectedImage || firstProduct.images[0].url" :alt="'error'" class="w-[50vh] h-auto" />
         </div>
       </div>
       <div class="flex flex-col justify-center gap-y-2">      
         <h1 class="text-xl mt-4 l:mt-0"><strong>{{ firstProduct.title }}</strong></h1>
         <p><strong>€ {{ firstProduct.variants[0].prices[0].amount }}</strong></p>
           <p class="text-xs"> {{ t("productHero.TasseESpedizione") }} </p>
-          <p class="text-xs mt-2 l:mt-0"><strong> {{ t("productHero.Lingua") }} :</strong></p>
-          <div class="flex flex-row gap-x-4 ">
-            <div v-for="variant in firstProduct.variants" :key="variant.id" >
-              <button class="h-10 px-2 border border-gray hover:mediumPink focus:mediumPink active:mediumPink">
-                <p>{{ variant.title }}</p>
-              </button>
-            </div>
-          </div>
-        <p class="text-xs mt-2 l:mt-0"><strong>{{ t("productHero.Quantita") }} :</strong></p>
+          <ChipLanguage 
+            :label="`${t('productHero.Lingua')} :`" 
+            :variants="firstProduct.variants" 
+            @variantSelected="handleVariantSelection"
+          />        
+          <p class="text-xs mt-2 l:mt-0"><strong>{{ t("productHero.Quantita") }} :</strong></p>
           <div class="flex justify-between">
-            <select class="h-10 px-8 border border-gray" v-model="selectedQuantity">
+            <select class="h-10 px-8 border border-gray rounded hover:border-mediumPink" v-model="selectedQuantity">
                 <option v-for="n in firstProduct.variants[0].inventory_quantity" :key="n" :value="n">{{ n }}</option>
             </select>
-            <button class="h-10 px-10 border border-gray">{{ t("productHero.AggiungiCarrello") }}</button>
+            <button class="h-10 px-10 border border-gray rounded hover:border-mediumPink">{{ t("productHero.AggiungiCarrello") }}</button>
           </div>
-          <button class="mt-2 h-10 w-full bg-mediumPink text-white">{{ t("productHero.AcquistaPaypal") }}</button>
-        <button class="text-s" @click="toggleDescription">{{ showDescription ? 'Mostra meno' : 'Mostra di più' }}</button>
+          <button class="mt-2 h-10 w-full bg-mediumPink text-white rounded hover:bg-darkPink">{{ t("productHero.AcquistaPaypal") }}</button>
+        <button class="text-s underline rounded" @click="toggleDescription">{{ showDescription ? t("productHero.MostraMeno") : t("productHero.MostraDiPiu") }}</button>
         <p v-if="showDescription"><strong>{{ t("productHero.Descrizione") }}:</strong> {{ firstProduct.description }}</p>
       </div>
     </div>
@@ -66,6 +63,10 @@
   
   function toggleDescription() {
       showDescription.value = !showDescription.value;
+  }
+
+  function handleVariantSelection(variant) {
+    console.log('Variant selezionato:', variant);
   }
   </script>
   
