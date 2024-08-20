e1739a7e1fe5e190ea94d1102c5ff4b57c5e8d5b
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-08-20 15:02:40 +0200

    Checkbox

---
+++ components/Atoms/Checkbox/Checkbox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="relative flex items-center">
+    <input
+      type="checkbox"
+      :id="id"
+      :checked="modelValue"
+      @change="handleCheckboxChange"
+      class="checkbox-input w-4 h-4 border border-accent-500 relative cursor-pointer hover:shadow appearance-none"
+    />
+    <label :for="id" class="absolute cursor-pointer" v-if="modelValue">
+      <Icon name="jig:check-accent" style="color: white"></Icon>
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps({
+  id: String,
+  modelValue: Boolean,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function handleCheckboxChange(event: Event) {
+  emit("update:modelValue", (event.target as HTMLInputElement).checked);
+}
+</script>
+
+<style scoped>
+.checkbox-input {
+  transition: all 0.2s;
+}
+</style>
--- pages/components-sample.vue
+++ pages/components-sample.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-y-6 pt-10 px-10">
+  <div class="flex flex-col gap-y-6 p-10">
     <div>
       <h3 class="pb-4">Atoms Cta</h3>
       <div class="flex flex-row gap-x-4">
@@ -68,6 +68,14 @@
         <AtomsLink to="/link-3">Link 3</AtomsLink>
       </div>
     </div>
+
+    <div>
+      <h1 class="text-xl pb-1">Checkbox</h1>
+      <p class="pb-4">
+        La checkbox è {{ isChecked ? "selezionata" : "deselezionata" }}.
+      </p>
+      <AtomsCheckbox id="example-checkbox" v-model="isChecked" />
+    </div>
   </div>
 </template>
 
@@ -75,6 +83,7 @@
 import { ref } from "vue";
 
 const radioButtonStatus = ref(true);
+const isChecked = ref(false);
 
 const updateState = (newState: boolean) => {
   radioButtonStatus.value = newState;
