586cf6dad253fc7af0087d72c7fb9f0dcf5d1df6
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-08-20 13:19:22 +0200

    Link

---
+++ components/Atoms/Link/Link.vue
@@ -0,0 +1,53 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="atoms-link"
+    :class="{ 'visited-link': isVisited }"
+    @click="handleClick"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+
+const isVisited = ref(false);
+
+onMounted(() => {
+  if (localStorage.getItem(props.to)) {
+    isVisited.value = true;
+  }
+});
+
+function handleClick() {
+  localStorage.setItem(props.to, "visited");
+  isVisited.value = true;
+}
+</script>
+
+<style scoped>
+.atoms-link {
+  @apply text-neutrals-950 no-underline;
+  font-family: "Roboto Flex", sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+}
+
+.atoms-link:hover {
+  @apply text-accent-500;
+}
+.atoms-link:active {
+  @apply bg-accent-500;
+}
+.visited-link {
+  @apply text-[#0EA5E9];
+}
+</style>
