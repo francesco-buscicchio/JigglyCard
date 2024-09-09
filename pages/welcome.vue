df065e01e7243cac3f162444c140f36865ced582
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-09-09 13:15:05 +0200

    Fix Welcome

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -3,25 +3,35 @@
     <div class="flex-grow flex flex-col items-center space-y-4 mt-4">
       <div class="w-full flex items-center justify-center relative">
         <div>
-          <h1 class="text-lightPink">JIGGLYCARD</h1>
+          <h1 class="text-accent-500">Jigglycard</h1>
         </div>
       </div>
-      <h2>{{ prestoOnline }}</h2>
     </div>
 
     <div
-      class="my-5 md:my-20 w-full flex flex-col md:flex-row justify-center items-center md:items-center md:space-x-4 space-y-4 md:space-y-0 md:flex-grow md:min-h-[50vh]">
-      <StaticNewsLetter class="order-2 md:order-1 w-full md:w-1/3 md:px-10 mt-4 md:mt-none" />
+      class="my-5 md:my-20 w-full flex flex-col md:flex-row justify-center items-center md:items-center md:space-x-4 space-y-4 md:space-y-0 md:flex-grow md:min-h-[50vh]"
+    >
+      <StaticNewsLetter
+        class="order-2 md:order-1 w-full md:w-1/3 md:px-10 mt-4 md:mt-none"
+      />
       <div class="px-5 md:pr-20 order-1 md:order-2 w-full md:w-2/3">
-        <img :src="Mainimg" alt="Main Image" class="w-full h-auto object-contain" />
+        <img
+          :src="Mainimg"
+          alt="Main Image"
+          class="w-full h-auto object-contain"
+        />
       </div>
     </div>
 
-    <OrganismsFooter :footer="footerData" :policyLinks="policyLinks" :hidePayments="true" :showInformationSite="true"
-      class="mt-auto" />
+    <OrganismsFooter
+      :footer="footerData"
+      :policyLinks="policyLinks"
+      :hidePayments="true"
+      :showInformationSite="true"
+      class="mt-auto"
+    />
   </div>
 </template>
-
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
@@ -29,12 +39,11 @@ import facebookLogo from "~/assets/icons/facebook.svg";
 import instagramLogo from "~/assets/icons/instagram.svg";
 import youtubeLogo from "~/assets/icons/youtube.svg";
 import tiktokLogo from "~/assets/icons/tiktok.png";
-import Ossidiana from "~/assets/img/Ossidiana.jpg";
+import WelcomeHero from "~/assets/img/welcomeHero.avif";
 
-const Mainimg: string = Ossidiana;
+const Mainimg: string = WelcomeHero;
 
 const { t } = useI18n();
-const prestoOnline = t("prestoOnline");
 
 definePageMeta({
   layout: false,
