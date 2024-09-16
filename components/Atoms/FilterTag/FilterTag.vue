d0e4bda9b2adc156b0f3aca8da93c480d076c72d
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-09-16 10:29:38 +0200

    Implementazione Tag

---
+++ components/Atoms/FilterTag/FilterTag.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="relative">
+    <div class="bg-accent-50 rounded-xl p-4 pr-12">
+      <p>{{ text }}</p>
+    </div>
+    <span
+      class="absolute inset-y-0 right-0 flex items-center pr-4"
+      @click="emitClick"
+    >
+      <Icon name="jig:close-filter" size="10" />
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from "vue";
+
+const emit = defineEmits(["removeFilter"]);
+const props = defineProps({
+  text: {
+    type: String,
+    default: "",
+  },
+});
+
+const emitClick = () => {
+  emit("removeFilter", props.text);
+};
+</script>
---
+++ pages/[category]/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full mx-8">
+    <OrganismsListingFilters></OrganismsListingFilters>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const data = [
+  { name: "Singole", url: `/${route.params.category}` },
+  {
+    name: "Zenith Regale",
+    url: `/${route.params.category}/${route.params.subcategory}`,
+  },
+  { name: "Pikachu (CRZ 160)" },
+];
+</script>
