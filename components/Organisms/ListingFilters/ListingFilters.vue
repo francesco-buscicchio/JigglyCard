6ab601009b7d03e67aad06e7bac67a8edc7be77e
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-09-16 11:13:53 +0200

    Implementazione Tag (#66)

---
+++ components/Organisms/ListingFilters/ListingFilters.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-row items-start flex-wrap gap-2">
+    <AtomsFilterTag
+      v-for="item of filterList"
+      :text="item"
+      @remove-filter="removeFilter"
+    ></AtomsFilterTag>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const filterList = ref([
+  "inglese",
+  "disponibile",
+  "carte singole",
+  "prima edizione",
+]);
+
+const removeFilter = (item: string) => {
+  filterList.value = filterList.value.filter((val) => val !== item);
+};
+</script>
