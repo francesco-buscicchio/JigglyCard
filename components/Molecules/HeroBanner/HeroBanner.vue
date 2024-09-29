e3839cc660d358e1e987113780531ba317c969f4
Gaetano Di Natale <gaetano.dinatale@apuliasoft.com>
2024-09-29 11:48:54 +0200

    fix

--- components/Molecules/HeroBanner/HeroBanner.vue
+++ components/Molecules/HeroBanner/HeroBanner.vue
@@ -1,51 +1,43 @@
 <template>
-  <div
-    class="relative bg-cover bg-center h-[300px] md:h-[500px] cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105"
-    :style="{ backgroundImage: `url(${backgroundImage})` }"
-    @click="navigateToListing"
-    role="img"
-    :aria-label="ariaLabel"
-  >
-    <div
-      class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center"
-    >
-      <h1 class="text-white text-3xl md:text-5xl font-bold text-center">
-        {{ title }}
-      </h1>
-    </div>
-  </div>
+  <swiper :modules="[Navigation, Pagination]" pagination>
+    <swiper-slide v-for="(slide, index) in slides" :key="index">
+      <div
+        class="relative bg-cover bg-center h-[300px] md:h-[500px] cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105"
+        :style="{ backgroundImage: `url(${slide.imageUrl})` }" @click="navigateToListing(slide.navigateTo)" role="img">
+        <div class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+          <h1 class="text-white text-3xl md:text-5xl font-bold text-center">
+            {{ slide.productName }}
+          </h1>
+        </div>
+      </div>
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/swiper-bundle.css";
+import { Navigation, Pagination } from "swiper/modules";
 
 const props = defineProps({
-  backgroundImage: {
-    type: String,
+  slides: {
+    type: Array,
     required: true,
-  },
-  navigateTo: {
-    type: String,
-    required: true,
-  },
-  title: {
-    type: String,
-    default: "Ossidiana Infuocata",
-  },
-  ariaLabel: {
-    type: String,
-    default: "Promotional banner for Ossidiana Infuocata expansion",
-  },
+    default: () => []
+  }
 });
 
-// Usa il router per la navigazione
 const router = useRouter();
 
-const navigateToListing = () => {
-  router.push(props.navigateTo);
+const navigateToListing = (route) => {
+  router.push(route);
 };
 </script>
 
 <style scoped>
-/* Aggiungi eventuali stili aggiuntivi qui, se necessario */
+.mySwiper {
+  width: 100%;
+  height: 100%;
+}
 </style>
