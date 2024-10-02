b93e9b8ff3519db260dbd4dd533364c7e2db33f5
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-02 23:20:00 +0200

    feature/listing-filters

---
+++ components/Molecules/ItemsCounter/ItemsCounter.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ `${startItem}-${endItem} ${$t("of")} ${totalItems}` }}</p>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  page: {
+    type: Number,
+    default: 1,
+  },
+  itemForPage: {
+    type: Number,
+    default: 1,
+  },
+  totalItems: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const startItem = computed(() => {
+  return (props.page - 1) * props.itemForPage + 1;
+});
+
+const endItem = computed(() => {
+  if (props.page * props.itemForPage > props.totalItems)
+    return props.totalItems;
+  else return props.page * props.itemForPage;
+});
+</script>
---
+++ components/Molecules/PageSorter/PageSorter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="relative max-w-40">
+    <select
+      @change="handleSortingValue"
+      class="bg-white border-[1px] border-accent-950 rounded-md shadow-sm w-full py-3 pl-4 pr-8 appearance-none"
+    >
+      <option v-for="item of sortingItems" :value="item.value">
+        {{ $t(item.name) }}
+      </option>
+    </select>
+    <div
+      class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-700"
+    >
+      <Icon name="jig:arrow-down" size="8"></Icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import sortingItems from "~/data/sorting";
+
+const emit = defineEmits(["handleSorting"]);
+
+const handleSortingValue = (e: Event) => {
+  const selectElement = e.target as HTMLSelectElement;
+  emit("handleSorting", selectElement.value);
+};
+</script>
