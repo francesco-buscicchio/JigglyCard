d66340e4bc4aceb47b184011c1a540ccb0c18289
GaetanoDiNatale <137878215+GaetanoDiNatale@users.noreply.github.com>
2024-10-08 23:34:40 +0200

    Feature filter (#85)

---
+++ pages/test.vue
@@ -0,0 +1,22 @@
+<template>
+    <br>
+
+    <OrganismsFilter></OrganismsFilter>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi natus ipsa quos obcaecati dicta reprehenderit
+        incidunt, corporis veritatis dolore doloribus officia blanditiis numquam? Explicabo vel omnis magni
+        reprehenderit! Quasi, repellendus?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi natus ipsa quos obcaecati dicta reprehenderit
+        incidunt, corporis veritatis dolore doloribus officia blanditiis numquam? Explicabo vel omnis magni
+        reprehenderit! Quasi, repellendus?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi natus ipsa quos obcaecati dicta reprehenderit
+        incidunt, corporis veritatis dolore doloribus officia blanditiis numquam? Explicabo vel omnis magni
+        reprehenderit! Quasi, repellendus?</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi natus ipsa quos obcaecati dicta reprehenderit
+        incidunt, corporis veritatis dolore doloribus officia blanditiis numquam? Explicabo vel omnis magni
+        reprehenderit! Quasi, repellendus?</p>
+    <br>
+</template>
+
+<script setup lang="ts">
+
+</script>
