e24169f303cf9e8f1c5d65e93d2d2f9d7c45433c
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-09 14:27:58 +0200

    feature/conteggio-prodotti (#86)
    * feature/conteggio-prodotti
    * PR Fix

--- data/const.ts
+++ data/const.ts
@@ -3,3 +3,4 @@ export const HIGHLIGHTS_TAG = "EVIDENZA";
 export const WHATSNEW_TAG = "NOVITA";
 export const DEALS_TAG = "OFFERTA";
 export const HEROBANNER_TAG = "HEROBANNER";
+export const ITEMS_FOR_PAGE = 9;
