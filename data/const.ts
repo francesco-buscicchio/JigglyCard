a68ce691df383ed96e2225c085b7c43128e0a946
SerenaGiumentaro <serena.giumentaro@gmail.com>
2024-10-14 13:48:21 +0200

    feat: creation footer links comp

--- data/const.ts
+++ data/const.ts
@@ -4,3 +4,8 @@ export const WHATSNEW_TAG = "NOVITA";
 export const DEALS_TAG = "OFFERTA";
 export const HEROBANNER_TAG = "HEROBANNER";
 export const ITEMS_FOR_PAGE = 9;
+export const FOOTER_MENU_ITEMS = [
+    { link: "about", route: "/about" },
+    { link: "spedizioni-resi-rimborsi", route: "/spedizioni" },
+    { link: "assistenza", route: "/assistenza" }
+  ];
