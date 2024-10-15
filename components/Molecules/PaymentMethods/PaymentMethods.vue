f4647aa9cadd601ecdb33ad8ac22b398029eaa33
SerenaGiumentaro <serena.giumentaro@gmail.com>
2024-10-15 17:55:31 +0200

    feat: removed unsed payments

--- components/Molecules/PaymentMethods/PaymentMethods.vue
+++ components/Molecules/PaymentMethods/PaymentMethods.vue
@@ -2,10 +2,10 @@
     <div>
         <p class="px-6 lg:text-center">{{ $t('payments.terms') }}</p>
         <div class="flex justify-center w-full">
-            <div class="grid grid-cols-4 m-4 px-4 max-w-[450px] gap-5">
+            <div class="flex m-4 px-4 gap-5">
                 <div v-for="(payment, index) in paymentMethods" :key="index" class="flex justify-center items-center">
                     <a :href="payment.link" @click="iconSocialPressed(payment.link)">
-                        <img :src="payment.src" :alt="payment.alt" class="mt-2 w-18 h-10" />
+                        <img :src="payment.src" :alt="payment.alt" class="mt-2 w-18 h-10 object-cover" />
                     </a>
                 </div>
             </div>
@@ -29,21 +29,21 @@ const paymentMethods: PaymentMethodType[] = [
         alt: "paypal",
         link: "#",
     },
-    {
-        src: "https://via.placeholder.com/100x50?text=Pay2",
-        alt: "Pay Method 2",
-        link: "#",
-    },
-    {
-        src: "https://via.placeholder.com/100x50?text=Pay3",
-        alt: "Pay Method 3",
-        link: "#",
-    },
-    {
-        src: "https://via.placeholder.com/100x50?text=Pay4",
-        alt: "Pay Method 4",
-        link: "#",
-    },
+    // {
+    //     src: "https://via.placeholder.com/100x50?text=Pay2",
+    //     alt: "Pay Method 2",
+    //     link: "#",
+    // },
+    // {
+    //     src: "https://via.placeholder.com/100x50?text=Pay3",
+    //     alt: "Pay Method 3",
+    //     link: "#",
+    // },
+    // {
+    //     src: "https://via.placeholder.com/100x50?text=Pay4",
+    //     alt: "Pay Method 4",
+    //     link: "#",
+    // },
 ];
 
 const iconSocialPressed = (url: string) => {
