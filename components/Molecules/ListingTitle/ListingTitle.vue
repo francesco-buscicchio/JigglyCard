33b3c1869b0211fb756bf4fc1d6cee218b42f745
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-24 23:43:10 +0200

    Titolo listing

---
+++ components/Molecules/ListingTitle/ListingTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-accent-500 text-center">{{ title?.split(" - ")[0] }}</h1>
+  <h5 class="text-accent-500 text-center pt-2">
+    {{ title?.split(" - ")[1].toLocaleLowerCase() }}
+  </h5>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+});
+</script>
