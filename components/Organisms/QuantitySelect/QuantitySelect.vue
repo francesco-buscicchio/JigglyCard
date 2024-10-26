338341b75c06a937746e33aa830eb26f345b3675
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-26 11:59:14 +0200

    Traduzioni

--- components/Organisms/QuantitySelect/QuantitySelect.vue
+++ components/Organisms/QuantitySelect/QuantitySelect.vue
@@ -1,39 +1,45 @@
 <template>
-    <div>
-        <div class="flex items-center justify-between">
-            <div class="flex mr-6 items-center">
-                <p class="mr-6">Quantità: </p>
-                <MoleculesPageSorter :sortingItems="quantityOptions" :selected="quantity"
-                    @handleSorting="updateQuantity" />
-            </div>
+  <div>
+    <div class="flex items-center justify-between">
+      <div class="flex mr-6 items-center">
+        <p class="mr-6">{{ $t("quantitySelect.quantity") }}:</p>
+        <MoleculesPageSorter
+          :sortingItems="quantityOptions"
+          :selected="quantity"
+          @handleSorting="updateQuantity"
+        />
+      </div>
 
-            <h2 class="mr-18">{{ totalPrice }} €</h2>
-        </div>
-
-        <p class="my-2">Disponibilità: {{ quantity }} pezzi</p>
+      <h2 class="mr-18">{{ totalPrice }} €</h2>
     </div>
+
+    <p class="my-2">
+      {{ $t("quantitySelect.availability") }}: {{ quantity }}
+      {{ $t("quantitySelect.pieces") }}
+    </p>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 const props = defineProps<{
-    quantity: number;
-    price: number;
+  quantity: number;
+  price: number;
 }>();
 
 const quantity = ref(1);
 
 const quantityOptions = computed(() => {
-    return Array.from({ length: props.quantity }, (_, i) => ({
-        value: (i + 1).toString(),
-        name: (i + 1).toString(),
-    }));
+  return Array.from({ length: props.quantity }, (_, i) => ({
+    value: (i + 1).toString(),
+    name: (i + 1).toString(),
+  }));
 });
 
 const totalPrice = computed(() => quantity.value * props.price);
 
 function updateQuantity(newQuantity: string) {
-    quantity.value = Number(newQuantity);
+  quantity.value = Number(newQuantity);
 }
 </script>
