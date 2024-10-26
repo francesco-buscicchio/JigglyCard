fc59b83f8a9ed55d8b7d9a917f218c19e798b133
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-26 16:26:16 +0200

    Fix Middleware

--- middleware/redirect.global.ts
+++ middleware/redirect.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to) => {
   const config = useRuntimeConfig();
   if (to.path !== "/welcome" && config.public.NODE_ENV !== "development") {
-    //return navigateTo("/welcome");
+    return navigateTo("/welcome");
   }
 });
