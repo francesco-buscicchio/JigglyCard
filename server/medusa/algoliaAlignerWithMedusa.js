4dcdcfa085c455f1a23faf3395bfce325248c0c5
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2024-10-26 15:07:35 +0200

    Fix for https

--- server/medusa/algoliaAlignerWithMedusa.js
+++ server/medusa/algoliaAlignerWithMedusa.js
@@ -1,11 +1,15 @@
 import fetch from "node-fetch";
+import https from "https";
 
 import { algoliasearch } from "algoliasearch";
 import { config } from "dotenv";
 
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 config();
 let url = process.env.SERVER_PRODUCTS;
-let settings = { method: "Get" };
 
 const client = algoliasearch(
   process.env.ALGOLIA_APPLICATION_ID,
@@ -13,7 +17,7 @@ const client = algoliasearch(
 );
 const indexName = "ecommerce";
 
-fetch(url, settings)
+fetch(url, { method: "Get", agent })
   .then((res) => res.json())
   .then(async (json) => {
     try {
