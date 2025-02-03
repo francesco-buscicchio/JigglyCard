d6b63b4d3c33b7ec592ab3f58310148d5836c8c1
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-02-03 14:00:52 +0100

    Fix

--- components/Organisms/SelectOptions/SelectOptions.vue
+++ components/Organisms/SelectOptions/SelectOptions.vue
@@ -1,33 +1,40 @@
 <template>
-    <div class="p-4">
-        <h5 class="pb-2">{{ $t('shippingMethod') }}</h5>
-        <div v-for="(option, index) in shippingOptions" :key="index" class="py-1">
-            <label class="flex items-center cursor-pointer">
-                <AtomsRadioButton type="radio" :value="option" v-model="selectedOption" :name="'shippingOptions'"
-                    @change="emitSelectedOption" />
-                <span class="ml-2">{{ option.name }}: {{ option.price }}€</span>
-            </label>
-        </div>
+  <div class="p-4">
+    <h5 class="pb-2">{{ $t("shippingMethod") }}</h5>
+    <div v-for="(option, index) in shippingOptions" :key="index" class="py-1">
+      <label class="flex items-center cursor-pointer">
+        <AtomsRadioButton
+          type="radio"
+          :value="option"
+          v-model="selectedOption"
+          :name="'shippingOptions'"
+          @change="emitSelectedOption"
+        />
+        <span class="ml-2 text-base"
+          >{{ option.name }}: {{ option.price }}€</span
+        >
+      </label>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineEmits, defineProps } from 'vue';
+import { ref, defineEmits, defineProps } from "vue";
 
 const props = defineProps({
-    shippingOptions: {
-        type: Array,
-        required: true,
-    },
+  shippingOptions: {
+    type: Array,
+    required: true,
+  },
 });
 
 const selectedOption = ref(null);
 
 const emit = defineEmits<{
-    (e: 'update:selectedOption', option: { name: string; price: number }): void;
+  (e: "update:selectedOption", option: { name: string; price: number }): void;
 }>();
 
 const emitSelectedOption = () => {
-    emit('update:selectedOption', selectedOption.value);
+  emit("update:selectedOption", selectedOption.value);
 };
 </script>
