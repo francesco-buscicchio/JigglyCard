0020d905be92acf3781bb0d3a847d8304754ea5f
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-02-03 15:32:52 +0100

    feature/header-desktop-no-search (#113)

--- components/Organisms/HeaderDesktop/HeaderDesktop.vue
+++ components/Organisms/HeaderDesktop/HeaderDesktop.vue
@@ -1,60 +1,30 @@
 <template>
-  <div class="bg-ultralitePink">
-    <div class="flex justify-between items-center bg-ultralitePink px-4">
-      <img
-        src="~/assets/img/logo.png"
-        alt="Logo"
-        style="width: 40px"
-        @click="handleLogoClick"
-        class="cursor-pointer"
-      />
-
-      <div class="flex-grow mx-4 w-full px-4 py-2">
-        <div class="relative">
-          <input
-            type="text"
-            placeholder="Cerca..."
-            class="input"
-            @input="filterResults"
-          />
-          <div
-            class="results-container absolute z-10 top-full left-0 right-0 bg-white shadow-md max-h-60 overflow-y-auto w-full"
-            v-show="filteredResults.length"
-          >
-            <div
-              class="p-2 hover:bg-ultralitePink"
-              v-for="(result, index) in filteredResults"
-              :key="index"
-            >
-              {{ result }}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="flex items-center gap-x-4">
-        <button class="mr-4 relative" aria-label="Carrello">
-          <img
-            src="~/assets/img/cart.png"
-            alt="Carrello"
-            class="min-h-6 min-w-6 max-h-6 max-w-6"
-          />
-          <span
-            class="absolute left-4 -top-4 rounded-full bg-red-500 text-white text-xs px-2 py-1"
-          >
-            {{ header.cartCount }}
-          </span>
-        </button>
-        <button>
-          <img
-            src="~/assets/img/user.png"
-            alt="Utente"
-            class="min-h-6 min-w-6 max-h-6 max-w-6"
-          />
-        </button>
-      </div>
+  <header
+    class="flex justify-between items-center p-5 bg-white shadow-md px-18"
+  >
+    <h2 @click="handleLogoClick()" class="text-accent-950">Jigglycard</h2>
+    <div class="flex flex-row gap-x-4">
+      <button
+        v-for="(item, index) of headerMenu"
+        @click="handleMenuItemClick(item.to)"
+      >
+        <h5 class="text-accent-950 text-lg">
+          {{ item.name }}
+        </h5>
+      </button>
     </div>
-  </div>
+    <div class="flex flex-row gap-x-4">
+      <button class="focus:outline-none">
+        <Icon name="jig:search-header-desktop" size="18" />
+      </button>
+      <button class="focus:outline-none">
+        <Icon name="jig:cart-header-desktop" size="18"></Icon>
+      </button>
+      <button class="focus:outline-none">
+        <Icon name="jig:user-accent" size="18" />
+      </button>
+    </div>
+  </header>
 </template>
 
 <script lang="ts" setup>
@@ -68,10 +38,21 @@ const props = defineProps<{ header: HeaderPropsType }>();
 
 const searchQuery = ref("");
 const searchResults = ref(["Prodotto 1", "Prodotto 2", "Prodotto 3"]);
+const headerMenu = ref([
+  { name: "Pokemon", to: "/pokemon/all" },
+  { name: "One Piece", to: "/one-piece/all" },
+  { name: "Final Fantasy", to: "/final-fantasy/all" },
+  { name: "Dragon Ball", to: "/dragon-ball/all" },
+  { name: "Lorcana", to: "/lorcana/all" },
+]);
 const filteredResults = ref([] as string[]);
 
 const handleLogoClick = () => {
   navigateTo("/");
+};
+
+const handleMenuItemClick = (item: string) => {
+  navigateTo(item);
 };
 
 function filterResults(event: Event) {
