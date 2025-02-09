4cbca2edd9ca7645463eb6e368e423c4c430de62
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-02-09 11:52:46 +0100

    refactor: add search result types

--- types/product.type.ts
+++ types/product.type.ts
@@ -9,3 +9,69 @@ export type ProductType = {
     category: string;
   };
   
+  export interface SearchProductResult {
+    hits: Hit[];
+    nbHits: number;
+    page: number;
+    nbPages: number;
+    hitsPerPage: number;
+    exhaustiveNbHits: boolean;
+    exhaustiveTypo: boolean;
+    exhaustive: {
+      nbHits: boolean;
+      typo: boolean;
+    };
+    query: string;
+    params: string;
+    renderingContent: Record<string, unknown>;
+    processingTimeMS: number;
+    processingTimingsMS: {
+      _request: {
+        roundTrip: number;
+      };
+      total: number;
+    };
+    serverTimeMS: number;
+  }
+  
+  export interface Hit {
+    name: string;
+    code: string;
+    shortDescription: string | null;
+    thumbnailImage: string;
+    salePrice: number;
+    tcg: string;
+    type: string;
+    images: string[];
+    expansion: string;
+    tags: string[];
+    variantsDetails: VariantDetail[];
+    languages: string[];
+    conditions: string[];
+    quantity: number;
+    available: boolean;
+    objectID: string;
+    _highlightResult: HighlightResult;
+  }
+  
+  interface VariantDetail {
+    id: string;
+    language: string;
+    condition: string;
+    price: number;
+  }
+  
+  interface HighlightResult {
+    name: {
+      value: string;
+      matchLevel: string;
+      fullyHighlighted: boolean;
+      matchedWords: string[];
+    };
+    tags: Array<{
+      value: string;
+      matchLevel: string;
+      matchedWords: string[];
+    }>;
+  }
+  
