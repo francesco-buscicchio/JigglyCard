ce477594d193b3c9f87cf95e5c496f2a41a470ab
SerenaGiumentaro <92537290+SerenaGiumentaro@users.noreply.github.com>
2025-02-10 19:58:14 +0100

    JIG#: Fix/research results (#115)

--- components/Molecules/SearchItemResult/SearchItemResult.vue
+++ components/Molecules/SearchItemResult/SearchItemResult.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="w-full py-2 border-b-[1px] flex-row flex px-6 gap-x-10">
-    <NuxtImg :src="thumbnailImage" class="min-w-10 max-w-10 object-cover" />
+  <div class="w-full py-2 border-b-[1px] flex px-6 gap-x-10">
+    <NuxtImg :src="thumbnailImage" class="min-w-10 max-w-10 object-cover" :alt="name"/>
     <div>
       <p>{{ name }}</p>
-      <p>{{ objectID }}</p>
+      <p>{{ expansion }}</p>
     </div>
   </div>
 </template>
@@ -19,6 +19,10 @@ const props = defineProps({
     required: true,
   },
   objectID: {
+    type: String,
+    required: true,
+  },
+  expansion: {
     type: String,
     required: true,
   },
