0fdc9bac3ee139c99b20b9c8253470a6896544cd
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-02-10 10:16:22 +0100

    fix: remove flex-row and adding alt image

--- components/Molecules/SearchItemResult/SearchItemResult.vue
+++ components/Molecules/SearchItemResult/SearchItemResult.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full py-2 border-b-[1px] flex-row flex px-6 gap-x-10">
-    <NuxtImg :src="thumbnailImage" class="min-w-10 max-w-10 object-cover" />
+  <div class="w-full py-2 border-b-[1px] flex px-6 gap-x-10">
+    <NuxtImg :src="thumbnailImage" class="min-w-10 max-w-10 object-cover" :alt="name"/>
     <div>
       <p>{{ name }}</p>
       <p>{{ expansion }}</p>
