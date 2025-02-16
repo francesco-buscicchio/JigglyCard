9788251b1d3b7d7e443a4f84a43125fea8cab937
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-02-16 16:18:39 +0100

    fat: add back to home link to thank you page

--- components/Molecules/ThankYou/ThankYou.vue
+++ components/Molecules/ThankYou/ThankYou.vue
@@ -3,6 +3,9 @@
     <h1 class="text-accent-500">{{ $t("thanks.main") }}</h1>
     <p class="pt-5">{{ $t(`thanks.${props.type}`) }}</p>
     <img class="lg:w-103 w-62 pt-10" :src="thanksSrc" alt="Thank You" />
+    <a href="/" class="text-accent-500 link-to-home my-10">{{
+      $t("thanks.backHome")
+    }}</a>
   </div>
 </template>
 
@@ -18,3 +21,12 @@ const props = defineProps({
   },
 });
 </script>
+
+<style>
+.link-to-home {
+  font-family: "Roboto Flex";
+  text-decoration: underline;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
