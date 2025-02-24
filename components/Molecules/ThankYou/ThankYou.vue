9acd15f0e04e2e69cae52bcf3a8edaa0cc6f038f
SerenaGiumentaro <92537290+SerenaGiumentaro@users.noreply.github.com>
2025-02-24 18:29:45 +0100

    feat: add back to home link to thank you page (#122)

--- components/Molecules/ThankYou/ThankYou.vue
+++ components/Molecules/ThankYou/ThankYou.vue
@@ -3,12 +3,18 @@
     <h1 class="text-accent-500">{{ $t("thanks.main") }}</h1>
     <p class="pt-5">{{ $t(`thanks.${props.type}`) }}</p>
     <img class="lg:w-103 w-62 pt-10" :src="thanksSrc" alt="Thank You" />
+    <AtomsButtonCTA
+      type="cart-drop"
+      :text="$t('thanks.backHome')"
+      @button-clicked="navigateToHome()"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { defineProps } from "vue";
 import thanksSrc from "@/assets/img/thanks.png";
+import { PATH } from "~/data/const";
 
 const props = defineProps({
   type: {
@@ -17,4 +23,17 @@ const props = defineProps({
     validator: (value: string) => ["order", "newsletter"].includes(value),
   },
 });
+
+const navigateToHome = () => {
+  navigateTo(PATH.HOME);
+};
 </script>
+
+<style>
+.link-to-home {
+  font-family: "Roboto Flex";
+  text-decoration: underline;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>
