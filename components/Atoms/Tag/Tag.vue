5b292b36094cecd2bd5b576747a0a826770ba2ed
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-03-17 13:37:49 +0100

    Update Tag.vue

--- components/Atoms/Tag/Tag.vue
+++ components/Atoms/Tag/Tag.vue
@@ -1,43 +1,53 @@
 <template>
-    <button :class="typeClass" class="rounded-lg px-4 py-2 min-w-25" @click="emitClick">
-        <p>{{ $t(`tag.${text}`) }}</p>
-    </button>
+  <button
+    :class="typeClass"
+    class="rounded-lg px-4 py-2 min-w-25"
+    @click="emitClick"
+  >
+    <p>{{ $t(`tag.${text}`) }}</p>
+  </button>
 </template>
+
 <!-- TODO mettere traduzioni per le lingue -->
 <script setup lang="ts">
-import type { TagCode } from '~/components/Molecules/ListingTag/ListingTag.types';
-import { TagType } from './tag.types';
+import type { TagCode } from "~/components/Molecules/ListingTag/ListingTag.types";
+import { TagType } from "./tag.types";
+
+type TagTypeValues = `${TagType}`;
 
 const props = defineProps({
-    type: {
-        type: String,
-        default: "active",
-        validator: (value: TagType) => [TagType.ACTIVE, TagType.INACTIVE, TagType.DISABLED].includes(value),
-    },
-    text: {
-        type: String,
-        default: "",
-    },
-    code: {
-        type: String as () => TagCode,
-        required: true
-    }
+  type: {
+    type: String as () => TagTypeValues,
+    default: "active",
+    validator: (value: TagType) =>
+      [TagType.ACTIVE, TagType.INACTIVE, TagType.DISABLED].includes(value),
+  },
+  text: {
+    type: String,
+    default: "",
+  },
+  code: {
+    type: String as () => TagCode,
+    required: true,
+  },
 });
 
 const emit = defineEmits(["tagClicked"]);
 
 const typeClass = computed(() => {
-    return {
-        "bg-accent-500 text-accent-50 font-bold active:bg-accent-950":
-            props.type === TagType.ACTIVE,
-        "bg-sky-100 text-primary-950 font-bold active:text-accent-950 active:border-accent-950":
-            props.type === TagType.INACTIVE,
-        "bg-white text-neutrals-400 border-[1px] border-neutrals-500":
-            props.type === TagType.DISABLED,
-    }
-})
+  switch (props.type) {
+    case TagType.ACTIVE:
+      return "bg-accent-500 text-accent-50 font-bold active:bg-accent-950";
+    case TagType.INACTIVE:
+      return "bg-sky-100 text-primary-950 font-bold active:text-accent-950 active:border-accent-950";
+    case TagType.DISABLED:
+      return "bg-white text-neutrals-400 border-[1px] border-neutrals-500";
+    default:
+      return "";
+  }
+});
 
 const emitClick = () => {
-    emit("tagClicked", props.code);
+  emit("tagClicked", props.code);
 };
 </script>
