cc2308b6d80ece08b6bb0a05b9fde4a2d5b3fee7
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-04-04 09:41:52 +0200

    feat: implement responsive design for CartCard component

--- components/Molecules/CartCard/CartCard.vue
+++ components/Molecules/CartCard/CartCard.vue
@@ -1,26 +1,51 @@
 <template>
-  <div class="flex flex-row items-top gap-4">
-    <img :src="image" class="w-18" />
-    <div>
+  <!-- mobile -->
+  <div v-show="isMobileview"> 
+    <div class="flex flex-row items-top gap-4">
+      <img :src="image" class="w-18" />
+      <div>
+        <h5 class="pb-2">{{ title }}</h5>
+        <p class="pb-2">{{ language }}</p>
+        <p class="pb-2">{{ condition }}</p>
+      </div>
+    </div>
+    <div class="pt-4">
+      <OrganismsQuantitySelect
+        :price="price"
+        :quantity="avaibleQuantity"
+        :isCart="true"
+      />
+    </div>
+    <div class="pt-7 pb-5">
+      <AtomsButtonCTA type="cart-drop" text="Elimina dal carrello" />
+    </div>
+  </div>
+  <!-- desktop -->
+  <div class="flex gap-3" v-show="!isMobileview">
+    <img :src="image" class="w-24" />
+
+    <div class="flex-1">
       <h5 class="pb-2">{{ title }}</h5>
       <p class="pb-2">{{ language }}</p>
       <p class="pb-2">{{ condition }}</p>
     </div>
-  </div>
-  <div class="pt-4">
-    <OrganismsQuantitySelect
-      :price="price"
-      :quantity="avaibleQuantity"
-      :isCart="true"
-    />
-  </div>
-  <div class="pt-7 pb-5">
-    <AtomsButtonCTA type="cart-drop" text="Elimina dal carrello" />
+    <div>
+      <div class="pt-4">
+        <OrganismsQuantitySelect
+          :price="price"
+          :quantity="avaibleQuantity"
+          :isCart="true"
+        />
+      </div>
+    </div>
+    <!-- elimina dal carrello -->
+    <Icon name="jig:close-accent" class="ml-6" size="20" @click=""></Icon>
   </div>
 </template>
 
 <script lang="ts" setup>
 const emit = defineEmits(["buttonClicked"]);
+const isMobileview = isMobile();
 
 defineProps<{
   image: string;
