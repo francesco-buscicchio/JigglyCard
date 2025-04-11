dbc416125c4e8aa4e7cf638b0a409d21e9c43fcb
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-04-11 10:31:11 +0200

    feat(ShippingMode): implement internationalization for shipping options and update UI text

--- components/Organisms/ShippingMode/ShippingMode.vue
+++ components/Organisms/ShippingMode/ShippingMode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-5 border-t-[2px] border-neutral-200 py-4">
-    <h5>Modalità di spedizione</h5>
+    <h5>{{ $t("shippingMode.shippingMethod") }}</h5>
 
     <div class="my-4">
       <AtomsRadioButton
@@ -15,25 +15,31 @@
     </div>
 
     <div class="flex justify-between border-t-[2px] border-neutral-200 py-4">
-      <p class="">Totale</p>
+      <p class="">{{ $t("shippingMode.total") }}</p>
       <p class="price-tag">{{ totalCart }} €</p>
     </div>
     <!-- Codice promo -->
     <div class="mb-12">
-      <p class="mb-2">Hai un codice promo?</p>
+      <p class="mb-2">{{ $t("shippingMode.couponCodeQuestion") }}</p>
       <div class="lg:flex w-full">
         <div class="mb-2">
           <AtomsInputText :placeholder="placeholder" />
         </div>
         <div class="lg:max-w-22">
-          <AtomsButtonCTA type="secondary" text="Applica Codice" />
+          <AtomsButtonCTA
+            type="secondary"
+            :text="$t('shippingMode.codeApply')"
+          />
         </div>
       </div>
     </div>
 
     <div class="flex flex-col gap-y-4">
-      <AtomsButtonCTA type="primary" text="Procedi all'acquisto" />
-      <AtomsButtonCTA type="underline-text" text="Torna ai prodotti" />
+      <AtomsButtonCTA type="primary" :text="$t('shippingMode.buyCTA')" />
+      <AtomsButtonCTA
+        type="underline-text"
+        :text="$t('shippingMode.goBackProducts')"
+      />
     </div>
 
     <div class="my-16">
@@ -57,11 +63,13 @@ const props = defineProps({
     required: true,
   },
 });
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 const isDesktopView = isDesktop();
 const placeholder = computed(() => {
-  return isDesktopView.value ? "Codice promo" : "XXXX";
+  return isDesktopView.value ? "XXXX" : t("shippingMode.couponCode");
 });
 // TODO: rendere di default l'opzione standard
 const selectedOption = ref({ id: "standard-1" });
 </script>
-
