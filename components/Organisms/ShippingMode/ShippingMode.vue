634e04361dfdcef5d9e54ea4179fe447b7347ca9
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-04-12 20:20:45 +0200

    fix(ShippingMode): replace $t with t for consistent internationalization usage

--- components/Organisms/ShippingMode/ShippingMode.vue
+++ components/Organisms/ShippingMode/ShippingMode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-5 border-t-[2px] border-neutral-200 py-4">
-    <h5>{{ $t("shippingMode.shippingMethod") }}</h5>
+    <h5>{{ t("shippingMode.shippingMethod") }}</h5>
 
     <div class="my-4">
       <AtomsRadioButton
@@ -10,17 +10,17 @@
       />
       <span class="ml-2 text-base">Standard 1€ </span>
       <p class="text-xs py-3">
-        {{ $t("shippingMode.message") }}
+        {{ t("shippingMode.message") }}
       </p>
     </div>
 
     <div class="flex justify-between border-t-[2px] border-neutral-200 py-4">
-      <p class="">{{ $t("shippingMode.total") }}</p>
+      <p class="">{{ t("shippingMode.total") }}</p>
       <p class="price-tag">{{ totalCart }} €</p>
     </div>
     <!-- Codice promo -->
     <div class="mb-12">
-      <p class="mb-2">{{ $t("shippingMode.couponCodeQuestion") }}</p>
+      <p class="mb-2">{{ t("shippingMode.couponCodeQuestion") }}</p>
       <div class="lg:flex w-full">
         <div class="mb-2">
           <AtomsInputText :placeholder="placeholder" />
@@ -28,28 +28,28 @@
         <div class="lg:max-w-22">
           <AtomsButtonCTA
             type="secondary"
-            :text="$t('shippingMode.codeApply')"
+            :text="t('shippingMode.codeApply')"
           />
         </div>
       </div>
     </div>
 
     <div class="flex flex-col gap-y-4">
-      <AtomsButtonCTA type="primary" :text="$t('shippingMode.buyCTA')" />
+      <AtomsButtonCTA type="primary" :text="t('shippingMode.buyCTA')" />
       <AtomsButtonCTA
         type="underline-text"
-        :text="$t('shippingMode.goBackProducts')"
+        :text="t('shippingMode.goBackProducts')"
       />
     </div>
 
     <div class="my-16">
       <MoleculesTextViewer>
         <template v-slot:title>
-          {{ $t("needAssistance") }}
+          {{ t("needAssistance") }}
         </template>
         <template v-slot:content>
           <!-- TODO mettere descrizione corretta -->
-          {{ $t("defaultDescription") }}
+          {{ t("defaultDescription") }}
         </template>
       </MoleculesTextViewer>
     </div>
