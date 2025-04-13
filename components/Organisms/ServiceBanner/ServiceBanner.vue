c32922872a68fd97bd90227e3c66c06c384a3082
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-04-13 09:49:27 +0200

    fix/sonarqube-fix

--- components/Organisms/ServiceBanner/ServiceBanner.vue
+++ components/Organisms/ServiceBanner/ServiceBanner.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="gap-4">
-    <div class="grid md:grid-cols-4 md:gap-4 lg:py-8 ">
-      <div v-for="(section, sectionIndex) in sections" :key="'section-' + sectionIndex" :class="{
-        'w-full md:w-auto md:mb-0 py-4 px-2 section-bar border-b-[1px] border-accent-950 md:border-none bg-accent-50': sectionIndex !== sections.length - 1,
-        'w-full md:w-auto md:mb-0 py-4 px-2 section-bar md:border-none bg-accent-50': sectionIndex === sections.length - 1
-      }" :style="{ fontSize: sectionFontSize }">
+    <div class="grid md:grid-cols-4 md:gap-4 lg:py-8">
+      <div
+        v-for="(section, sectionIndex) in sections"
+        :key="'section-' + sectionIndex"
+        :class="{
+          'w-full md:w-auto md:mb-0 py-4 px-2 section-bar border-b-[1px] border-accent-950 md:border-none bg-accent-50':
+            sectionIndex !== sections.length - 1,
+          'w-full md:w-auto md:mb-0 py-4 px-2 section-bar md:border-none bg-accent-50':
+            sectionIndex === sections.length - 1,
+        }"
+        :style="{ fontSize: sectionFontSize }"
+      >
         <div class="flex flex-row items-center justify-between md:items-start">
           <div v-if="section.imgUrl" class="w-full flex justify-center">
             <Icon :name="section.imgUrl" size="35" />
@@ -13,14 +20,18 @@
             <p class="pb-2">{{ section.title }}</p>
             <div v-if="Array.isArray(section.sections)" class="pb-2">
               <div v-for="(item, index) in section.sections" :key="index">
-                <label v-if="typeof item === 'object'" class="line-clamp-3">
+                <label
+                  v-if="typeof item === 'object'"
+                  class="line-clamp-3"
+                  for="item.value"
+                >
                   <a :href="item.link">{{ item.value }}</a>
                 </label>
-                <label v-else class="line-clamp-3">{{ item }}</label>
+                <label v-else class="line-clamp-3" for="item">{{ item }}</label>
               </div>
             </div>
             <div v-else>
-              <label>{{ section.sections }}</label>
+              <label for="section.sections">{{ section.sections }}</label>
             </div>
           </div>
         </div>
