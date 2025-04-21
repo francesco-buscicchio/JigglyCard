aad63bcfd62e13cd7bd13d009d8747413ef7de55
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-04-21 19:37:52 +0200

    feat: ServiceBanner desktop layout

--- components/Organisms/ServiceBanner/ServiceBanner.vue
+++ components/Organisms/ServiceBanner/ServiceBanner.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="px-4 py-18 md:py-25 md:px-0">
-
-    <div class="gap-4 md:bg-accent-50 px-4">
-      <div class="grid md:grid-cols-4 md:gap-4 lg:py-8">
+    <!-- mobile -->
+    <div class="gap-4 px-4" v-show="isMobileView">
+      <div class="grid lg:py-8">
         <div
           v-for="(section, sectionIndex) in sections"
           :key="'section-' + sectionIndex"
           :class="{
-            'w-full md:w-auto md:mb-0 py-4 px-2 section-bar border-b-[1px] border-accent-950 md:border md:rounded-lg bg-accent-50':
+            'w-full py-4 px-2 section-bar border-b-[1px] border-accent-950 bg-accent-50':
               sectionIndex !== sections.length - 1,
-            'w-full md:w-auto md:mb-0 py-4 px-2 section-bar bg-accent-50 md:border md:border-accent-950 md:rounded-lg':
+            'w-full py-4 px-2 section-bar bg-accent-50 md:border':
               sectionIndex === sections.length - 1,
           }"
           :style="{ fontSize: sectionFontSize }"
         >
-          <div class="flex flex-row items-center justify-between md:items-start">
+          <div class="flex flex-row items-center justify-between">
             <div v-if="section.imgUrl" class="w-full flex justify-center">
               <Icon :name="section.imgUrl" size="35" />
             </div>
@@ -36,12 +36,52 @@
         </div>
       </div>
     </div>
+    <!-- desktop -->
+    <div v-show="!isMobileView" class="gap-4 bg-accent-50 px-4">
+      <div class="grid grid-cols-4 gap-4 lg:py-8">
+        <div
+          v-for="(section, sectionIndex) in sections"
+          :key="'section-' + sectionIndex"
+          :class="{
+            'w-auto mb-0 py-4 px-2 section-bar border-b-[1px] border-accent-950 border rounded-lg bg-accent-50':
+              sectionIndex !== sections.length - 1,
+            'w-auto mb-0 py-4 px-2 section-bar bg-accent-50 border border-accent-950 rounded-lg':
+              sectionIndex === sections.length - 1,
+          }"
+          :style="{ fontSize: sectionFontSize }"
+        >
+          <div class="flex flex-col justify-between items-start">
+            <p class="pb-2 w-full text-center">{{ section.title }}</p>
+
+            <div class="w-full flex px-3 gap-4">
+              <div
+                v-if="section.imgUrl"
+                class="flex justify-center items-center"
+              >
+                <Icon :name="section.imgUrl" size="40" />
+              </div>
+              <div v-if="Array.isArray(section.sections)">
+                <div v-for="(item, index) in section.sections" :key="index">
+                  <label v-if="typeof item === 'object'" class="line-clamp-3">
+                    <a :href="item.link">{{ item.value }}</a>
+                  </label>
+                  <label v-else class="line-clamp-3">{{ item }}</label>
+                </div>
+              </div>
+              <div v-else>
+                <label>{{ section.sections }}</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { t } = useI18n();
-
+const isMobileView = isMobile();
 const sections = [
   {
     title: t("serviceHomepage.FastShipping"),
