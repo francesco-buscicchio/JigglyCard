f9ceaaa91ce181419d8bba241b9bbc06e16a7ebf
SerenaGiumentaro <serena.giumentaro@gmail.com>
2025-05-18 11:18:01 +0200

    feat: refactor QuantitySelect component layout and remove unused isCart prop

--- components/Organisms/QuantitySelect/QuantitySelect.vue
+++ components/Organisms/QuantitySelect/QuantitySelect.vue
@@ -1,40 +1,19 @@
 <template>
   <div>
-    <div class="flex items-center gap-13">
-      <div class="flex mr-6 my-7 items-center gap-3">
-        <div>
-          <p class="mr-6">{{ t("quantitySelect.quantity") }}:</p>
-          <p class="my-1 text-xs" v-show="isDesktopView && !isCart">
-            ({{ t("quantitySelect.availability") }}: {{ quantity }}
-            {{ t("quantitySelect.pieces") }} )
-          </p>
-        </div>
+    <div class="flex items-center gap-13 lg:gap-4 lg:flex-col">
+      <div
+        class="flex mr-6 items-center lg:flex-col lg:mr-0 justify-center lg:gap-2"
+      >
+        <p class="mr-6 lg:mr-0">{{ t("quantitySelect.quantity") }}:</p>
         <MoleculesPageSorter
           :sortingItems="quantityOptions"
           :selected="quantity"
           @handleSorting="updateQuantity"
+          type="slim"
         />
       </div>
 
-      <h2 v-show="!isDesktopView" :class="!isCart ? 'mr-10' : ''" class="price">
-        {{ totalPrice }} €
-      </h2>
-    </div>
-
-    <p class="my-2" v-show="!isDesktopView && !isCart">
-      {{ t("quantitySelect.availability") }}: {{ quantity }}
-      {{ t("quantitySelect.pieces") }}
-    </p>
-    <div v-show="isDesktopView" class="flex gap-16 mt-6 items-center">
-      <h2 class="whitespace-nowrap price-tag text-3xl">{{ totalPrice }} €</h2>
-
-      <AtomsButtonCTA
-        type="primary"
-        :text="t('productHero.AddToCart')"
-        class="max-w-[30rem]"
-      >
-        <Icon name="jig:cart-white" size="30"></Icon>
-      </AtomsButtonCTA>
+      <h2 class="mr-10 lg:mr-0 lg:text-2xl">{{ totalPrice }} €</h2>
     </div>
   </div>
 </template>
@@ -44,7 +23,6 @@ const isDesktopView = isDesktop();
 const props = defineProps<{
   quantity: number;
   price: number;
-  isCart: boolean;
 }>();
 
 const quantity = ref(1);
