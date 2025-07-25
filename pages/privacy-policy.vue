ded800c22e78973f209829f5d3d914596ffaa39d
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-07-25 07:15:55 +0200

    Feature/privacy policy page (#169)

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="px-20 py-10 flex flex-col gap-y-6">
+    <div>
+      <h2>Titolare del Trattamento dei Dati</h2>
+      <p>
+        Jigglycard di Francesco Buscicchio - Via Principe Amedeo 94, Taranto<br />
+        Indirizzo email del Titolare: jigglycard@gmail.com
+      </p>
+    </div>
+
+    <h3>Tipo di Dati che raccogliamo</h3>
+    <p>
+      Il Titolare non fornisce una lista di tipologie di Dati Personali
+      raccolti.
+    </p>
+    <p>
+      Dettagli completi su ciascuna tipologia di Dati Personali raccolti sono
+      forniti nelle sezioni dedicate di questa privacy policy o mediante
+      specifici testi informativi visualizzati prima della raccolta dei Dati
+      stessi.
+    </p>
+    <p>
+      I Dati Personali possono essere liberamente forniti dall'Utente o, nel
+      caso di Dati di Utilizzo, raccolti automaticamente durante l'uso di questa
+      Applicazione.
+    </p>
+    <p>
+      Se non diversamente specificato, tutti i Dati richiesti da questa
+      Applicazione sono obbligatori. Se l’Utente rifiuta di comunicarli,
+      potrebbe essere impossibile per questa Applicazione fornire il Servizio.
+      Nei casi in cui questa Applicazione indichi alcuni Dati come facoltativi,
+      gli Utenti sono liberi di astenersi dal comunicare tali Dati, senza che
+      ciò abbia alcuna conseguenza sulla disponibilità del Servizio o sulla sua
+      operatività.
+    </p>
+    <p>
+      Gli Utenti che dovessero avere dubbi su quali Dati siano obbligatori sono
+      incoraggiati a contattare il Titolare.
+    </p>
+    <p>
+      L’eventuale utilizzo di Cookie - o di altri strumenti di tracciamento - da
+      parte di questa Applicazione o dei titolari dei servizi terzi utilizzati
+      da questa Applicazione ha la finalità di fornire il Servizio richiesto
+      dall'Utente, oltre alle ulteriori finalità descritte nel presente
+      documento e nella Cookie Policy.
+    </p>
+    <p>
+      L'Utente si assume la responsabilità dei Dati Personali di terzi ottenuti,
+      pubblicati o condivisi mediante questa Applicazione.
+    </p>
+
+    <h3>Modalità e luogo del trattamento dei Dati raccolti</h3>
+
+    <h4>Modalità di trattamento</h4>
+    <p>
+      Il Titolare adotta le opportune misure di sicurezza volte ad impedire
+      l’accesso, la divulgazione, la modifica o la distruzione non autorizzate
+      dei Dati Personali.
+    </p>
+    <p>
+      Il trattamento viene effettuato mediante strumenti informatici e/o
+      telematici, con modalità organizzative e con logiche strettamente
+      correlate alle finalità indicate. Oltre al Titolare, in alcuni casi,
+      potrebbero avere accesso ai Dati altri soggetti coinvolti
+      nell’organizzazione di questa Applicazione (personale amministrativo,
+      commerciale, marketing, legali, amministratori di sistema) ovvero soggetti
+      esterni (come fornitori di servizi tecnici terzi, corrieri postali,
+      hosting provider, società informatiche, agenzie di comunicazione) nominati
+      anche, se necessario, Responsabili del Trattamento da parte del Titolare.
+      L’elenco aggiornato dei Responsabili potrà sempre essere richiesto al
+      Titolare del Trattamento.
+    </p>
+
+    <h4>Luogo</h4>
+    <p>
+      I Dati sono trattati presso le sedi operative del Titolare ed in ogni
+      altro luogo in cui le parti coinvolte nel trattamento siano localizzate.
+      Per ulteriori informazioni, contatta il Titolare.
+    </p>
+    <p>
+      I Dati Personali dell’Utente potrebbero essere trasferiti in un paese
+      diverso da quello in cui l’Utente si trova. Per ottenere ulteriori
+      informazioni sul luogo del trattamento l’Utente può fare riferimento alla
+      sezione relativa ai dettagli sul trattamento dei Dati Personali.
+    </p>
+
+    <h4>Periodo di conservazione</h4>
+    <p>
+      Se non diversamente indicato in questo documento, i Dati Personali sono
+      trattati e conservati per il tempo richiesto dalla finalità per la quale
+      sono stati raccolti e potrebbero essere conservati per un periodo più
+      lungo a causa di eventuali obbligazioni legali o sulla base del consenso
+      degli Utenti.
+    </p>
+
+    <h3>Cookie Policy</h3>
+    <p>
+      Questa Applicazione fa utilizzo di Strumenti di Tracciamento. Per saperne
+      di più, gli Utenti possono consultare la
+      <a href="/cookie-policy">Cookie Policy</a>.
+    </p>
+
+    <h3>Ulteriori informazioni per gli utenti nell'Unione Europea</h3>
+
+    <h4>Base giuridica del trattamento</h4>
+    <p>
+      Il Titolare tratta Dati Personali relativi all’Utente in caso sussista una
+      delle seguenti condizioni:
+    </p>
+    <ul>
+      <li>
+        l’Utente ha prestato il consenso per una o più finalità specifiche;
+      </li>
+      <li>
+        il trattamento è necessario all'esecuzione di un contratto con l’Utente
+        e/o all'esecuzione di misure precontrattuali;
+      </li>
+      <li>
+        il trattamento è necessario per adempiere un obbligo legale al quale è
+        soggetto il Titolare;
+      </li>
+      <li>
+        il trattamento è necessario per l'esecuzione di un compito di interesse
+        pubblico o per l'esercizio di pubblici poteri di cui è investito il
+        Titolare;
+      </li>
+      <li>
+        il trattamento è necessario per il perseguimento del legittimo interesse
+        del Titolare o di terzi.
+      </li>
+    </ul>
+    <p>
+      È comunque sempre possibile richiedere al Titolare di chiarire la concreta
+      base giuridica di ciascun trattamento ed in particolare di specificare se
+      il trattamento sia basato sulla legge, previsto da un contratto o
+      necessario per concludere un contratto.
+    </p>
+
+    <h4>Ulteriori informazioni sul tempo di conservazione</h4>
+    <p>
+      Se non diversamente indicato in questo documento, i Dati Personali sono
+      trattati e conservati per il tempo richiesto dalla finalità per la quale
+      sono stati raccolti e potrebbero essere conservati per un periodo più
+      lungo a causa di eventuali obbligazioni legali o sulla base del consenso
+      degli Utenti.
+    </p>
+    <p>Pertanto:</p>
+    <ul>
+      <li>
+        I Dati Personali raccolti per scopi collegati all’esecuzione di un
+        contratto tra il Titolare e l’Utente saranno trattenuti sino a quando
+        sia completata l’esecuzione di tale contratto.
+      </li>
+      <li>
+        I Dati Personali raccolti per finalità riconducibili all’interesse
+        legittimo del Titolare saranno trattenuti sino al soddisfacimento di
+        tale interesse. L’Utente può ottenere ulteriori informazioni in merito
+        all’interesse legittimo perseguito dal Titolare nelle relative sezioni
+        di questo documento o contattando il Titolare.
+      </li>
+      <li>
+        Quando il trattamento è basato sul consenso dell’Utente, il Titolare può
+        conservare i Dati Personali più a lungo sino a quando detto consenso non
+        venga revocato. Inoltre, il Titolare potrebbe essere obbligato a
+        conservare i Dati Personali per un periodo più lungo per adempiere ad un
+        obbligo di legge o per ordine di un’autorità.
+      </li>
+    </ul>
+
+    <h3>
+      Diritti dell’Utente sulla base del Regolamento Generale sulla Protezione
+      dei Dati (GDPR)
+    </h3>
+    <p>
+      Gli Utenti possono esercitare determinati diritti con riferimento ai Dati
+      trattati dal Titolare.
+    </p>
+    <ul>
+      <li>revocare il consenso in ogni momento;</li>
+      <li>opporsi al trattamento dei propri Dati;</li>
+      <li>accedere ai propri Dati;</li>
+      <li>verificare e chiedere la rettificazione;</li>
+      <li>ottenere la limitazione del trattamento;</li>
+      <li>ottenere la cancellazione o rimozione dei propri Dati Personali;</li>
+      <li>ricevere i propri Dati o farli trasferire ad altro titolare;</li>
+      <li>proporre reclamo.</li>
+    </ul>
+
+    <h3>Come esercitare i diritti</h3>
+    <p>
+      Eventuali richieste di esercizio dei diritti dell'Utente possono essere
+      indirizzate al Titolare attraverso i recapiti forniti in questo documento.
+      La richiesta è gratuita e il Titolare risponderà nel più breve tempo
+      possibile, in ogni caso entro un mese.
+    </p>
+
+    <h3>Ulteriori informazioni sul trattamento</h3>
+    <h4>Difesa in giudizio</h4>
+    <p>
+      I Dati Personali dell’Utente possono essere utilizzati da parte del
+      Titolare in giudizio o nelle fasi preparatorie alla sua eventuale
+      instaurazione per la difesa da abusi nell'utilizzo di questa Applicazione
+      o dei Servizi connessi da parte dell’Utente.
+    </p>
+    <h4>Informative specifiche</h4>
+    <p>
+      Su richiesta dell’Utente, in aggiunta alle informazioni contenute in
+      questa privacy policy, questa Applicazione potrebbe fornire all'Utente
+      delle informative aggiuntive e contestuali riguardanti Servizi specifici,
+      o la raccolta ed il trattamento di Dati Personali.
+    </p>
+    <h4>Log di sistema e manutenzione</h4>
+    <p>
+      Per necessità legate al funzionamento ed alla manutenzione, questa
+      Applicazione e gli eventuali servizi terzi da essa utilizzati potrebbero
+      raccogliere log di sistema, ossia file che registrano le interazioni e che
+      possono contenere anche Dati Personali, quali l’indirizzo IP Utente.
+    </p>
+
+    <h3>Informazioni non contenute in questa policy</h3>
+    <p>
+      Ulteriori informazioni in relazione al trattamento dei Dati Personali
+      potranno essere richieste in qualsiasi momento al Titolare del Trattamento
+      utilizzando gli estremi di contatto.
+    </p>
+
+    <h3>Modifiche a questa privacy policy</h3>
+    <p>
+      Il Titolare del Trattamento si riserva il diritto di apportare modifiche
+      alla presente privacy policy in qualunque momento notificandolo agli
+      Utenti su questa pagina e, se possibile, su questa Applicazione.
+    </p>
+
+    <h3>Definizioni e riferimenti legali</h3>
+    <p>
+      <strong>Dati Personali (o Dati)</strong> - Costituisce dato personale
+      qualunque informazione che, direttamente o indirettamente, rende
+      identificata o identificabile una persona fisica.
+    </p>
+    <p>
+      <strong>Dati di Utilizzo</strong> - Sono le informazioni raccolte
+      automaticamente attraverso questa Applicazione.
+    </p>
+    <p>
+      <strong>Utente</strong> - L'individuo che utilizza questa Applicazione.
+    </p>
+    <p>
+      <strong>Interessato</strong> - La persona fisica cui si riferiscono i Dati
+      Personali.
+    </p>
+    <p>
+      <strong>Responsabile del Trattamento (o Responsabile)</strong> - La
+      persona fisica o giuridica che tratta dati personali per conto del
+      Titolare.
+    </p>
+    <p>
+      <strong>Titolare del Trattamento (o Titolare)</strong> - La persona fisica
+      o giuridica che determina le finalità e i mezzi del trattamento dei dati.
+    </p>
+    <p>
+      <strong>Questa Applicazione</strong> - Lo strumento hardware o software
+      tramite cui vengono raccolti i Dati Personali.
+    </p>
+    <p>
+      <strong>Servizio</strong> - Il Servizio fornito da questa Applicazione.
+    </p>
+    <p>
+      <strong>Unione Europea (o UE)</strong> - Ogni riferimento all’Unione
+      Europea in questo documento si intende esteso a tutti gli stati membri
+      dell'Unione Europea e dello Spazio Economico Europeo.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
